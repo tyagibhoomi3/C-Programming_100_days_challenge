3751f3ef5cc87f7f38e03c6ca230ce950326d063
tyagibhoomi3 <tyagibhoomi3@gmail.com>
2025-09-26 22:56:23 +0530

    Add files via upload

---
+++ Day-8/Q16.c
@@ -0,0 +1,24 @@
+//Q16: Write a program to input three numbers and find the largest among them using if–else.
+
+#include <stdio.h>
+int main()
+{
+	printf("Name - Bhoomi Tyagi\n SAP ID - 590028798\n Course - BCA\n Batch - 06\n");
+    printf("--------------------------------------------------\n");
+    int a, b, c;
+    printf("Enter three numbers: ");
+    scanf("%d %d %d", &a, &b, &c);
+    if (a >= b && a >= c)
+    {
+        printf("Largest is %d\n", a);
+    }
+    else if (b >= a && b >= c)
+    {
+        printf("Largest is %d\n", b);
+    }
+    else
+    {
+        printf("Largest is %d\n", c);
+    }
+return 0;
+}

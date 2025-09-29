1ee70212624d5c487502c30738be9f23cb216ca9
tyagibhoomi3 <tyagibhoomi3@gmail.com>
2025-09-30 01:35:07 +0530

    Add files via upload

---
+++ Day-28/Q55.C
@@ -0,0 +1,29 @@
+//Q55: Write a program to print all the prime numbers from 1 to n.
+
+#include <stdio.h>
+
+int main() {
+	printf("Name : Bhoomi Tyagi \n Sap_id : 590028798 \n Course : BCA \n Batch : 6 \n");
+	printf("------------------------------------------- \n");
+    int n, i, j, isPrime;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    printf("Prime numbers from 1 to %d are:\n", n);
+
+    for (i = 2; i <= n; i++) {
+        isPrime = 1; 
+        for (j = 2; j <= i / 2; j++) {
+            if (i % j == 0) {
+                isPrime = 0; 
+                break;
+            }
+        }
+        if (isPrime == 1) {
+            printf("%d ", i);
+        }
+   }
+   return 0;
+}
+
---
+++ Day-28/Q56.C
@@ -0,0 +1,25 @@
+//Q56: Read and print elements of a one-dimensional array
+
+#include <stdio.h>
+
+int main() {
+	printf("Name : Bhoomi Tyagi \n Sap_id : 590028798 \n Course : BCA \n Batch : 6 \n");
+	printf("------------------------------------------- \n");
+    int n, i;
+    int arr[100]; 
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Array elements are:\n");
+    for (i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
+

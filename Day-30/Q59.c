0df78d256b8fe20943c7b42de1d1ac129436ae12
tyagibhoomi3 <tyagibhoomi3@gmail.com>
2025-09-30 01:35:55 +0530

    Add files via upload

---
+++ Day-30/Q59.c
@@ -0,0 +1,31 @@
+//Count even and odd numbers in an array.
+#include <stdio.h>
+int main() 
+{
+    printf("Name - Shabdi Srivastava\n SAP ID - 590021135\n Course - BCA\n Batch - 06\n");
+    printf("--------------------------------------------------\n");
+    int n;
+    printf("Enter the number of elements in the array : ");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter %d elements : ",n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int even=0,odd=0;
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            even++;
+        }
+        else
+        {
+            odd++;
+        }
+    }
+    printf("Total even numbers : %d\n",even);
+    printf("Total odd numbers : %d\n",odd);
+    return 0;
+}
---
+++ Day-30/Q60.c
@@ -0,0 +1,35 @@
+//Count positive, negative, and zero elements in an array.
+#include <stdio.h>
+int main() 
+{
+    printf("Name - Shabdi Srivastava\n SAP ID - 590021135\n Course - BCA\n Batch - 06\n");
+    printf("--------------------------------------------------\n");
+    int n,zero=0,pos=0,neg=0;
+    printf("Enter the number of elements in the array : ");
+    scanf("%d",&n);
+    int arr[n];
+    printf("Enter %d elements : ",n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        if(arr[i]==0)
+        {
+            zero++;
+        }
+        else if(arr[i]>0)
+        {
+            pos++;
+        }
+        else if(arr[i]<0)
+        {
+            neg++;
+        }
+    }
+    printf("number of  positive elements is %d\n",pos);
+    printf("number of  negative elements is %d\n",neg);
+    printf("number of  zero elements is %d\n",zero);
+    return 0;
+}
